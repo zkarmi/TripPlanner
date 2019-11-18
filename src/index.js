f02fc8bba1a8e28894cfc27b07bb98d9d4423bcd
Emmanuel Kandilas <Ekandilas@gmail.com>
2019-11-18 12:49:58 -0500

    map error

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('is this working?')
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWthbmRpbGFzIiwiYSI6ImNrMzRwc2JjczAzd3IzY29heXUzdGt2YngifQ.mvLyO-qIYEaq6hFuCqHRXw';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
